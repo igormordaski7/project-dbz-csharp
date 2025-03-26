dbadef05dcdbb247f46ce86e1c0d8cf1cd1747f0
Igor Mordaski <igormordaski9@gmail.com>
2025-03-26 19:31:04 -0300

    Incluindo os primeiros códigos desenvolvido na sala

---
+++ Models/Personagem.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Xablau.Models
+{
+    public class Personagem
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+    }
+}
